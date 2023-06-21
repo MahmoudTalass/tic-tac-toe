8b59e35e05544fcf2eea587f4d68e23f40015b98
Mahmoud <mahmoud.talass@gmail.com>
2023-06-20 21:12:14 -0400

    Create module to render game content

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-let gameBoard = (() => {
-    let Gameboard = {
-        gameboard: [],
-    }
-
-    function renderGameBoard() {
-        
-    }
+const gameBoard = (() => {
+   let Gameboard = {
+      gameBoard: [],
+   };
 })();
 
+const renderGame = (() => {
+   const gameBoardContainer = document.querySelector("#gameBoard");
+
+   function renderGameBoard() {
+      for (let i = 1; i <= 9; i++) {
+         const spot = document.createElement("div");
+         spot.textContent = i;
+         spot.dataset.spot = i;
+         spot.classList.add("spot");
+         gameBoardContainer.appendChild(spot);
+      }
+   }
+
+   return {
+      renderGameBoard,
+   };
+})();
+
+renderGame.renderGameBoard();
